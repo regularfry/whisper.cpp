d949cf8b1cd39d9efc71f317f5e89793e6db02cb
regularfry <alex@blackkettle.org>
2023-08-09 16:20:00 +0100

    Port to pocketfft, nicking all the details from https://github.com/ggerganov/whisper.cpp/pull/583

--- whisper.cpp
+++ whisper.cpp
@@ -8,6 +8,7 @@
 #endif
 
 #include "ggml.h"
+#include "pocketfft.h"
 
 #include <algorithm>
 #include <cassert>
@@ -2347,146 +2348,93 @@ static std::string to_timestamp(int64_t t, bool comma = false) {
     return std::string(buf);
 }
 
-// naive Discrete Fourier Transform
-// input is real-valued
-// output is complex-valued
-static void dft(const std::vector<float> & in, std::vector<float> & out) {
-    int N = in.size();
-
-    out.resize(N*2);
-
-    for (int k = 0; k < N; k++) {
-        float re = 0;
-        float im = 0;
-
-        for (int n = 0; n < N; n++) {
-            float angle = 2*M_PI*k*n/N;
-            re += in[n]*cos(angle);
-            im -= in[n]*sin(angle);
-        }
-
-        out[k*2 + 0] = re;
-        out[k*2 + 1] = im;
-    }
+template <typename T>
+static inline T *fft_alloc(size_t size) {
+    return reinterpret_cast<T *>(pocketfft::detail::aligned_alloc((size_t)16, size * sizeof(T)));
 }
 
-// Cooley-Tukey FFT
-// poor man's implementation - use something better
-// input is real-valued
-// output is complex-valued
-static void fft(const std::vector<float> & in, std::vector<float> & out) {
-    out.resize(in.size()*2);
-
-    int N = in.size();
-
-    if (N == 1) {
-        out[0] = in[0];
-        out[1] = 0;
-        return;
-    }
-
-    if (N%2 == 1) {
-        dft(in, out);
-        return;
-    }
-
-    std::vector<float> even;
-    std::vector<float> odd;
-
-    even.reserve(N/2);
-    odd.reserve(N/2);
-
-    for (int i = 0; i < N; i++) {
-        if (i % 2 == 0) {
-            even.push_back(in[i]);
-        } else {
-            odd.push_back(in[i]);
-        }
-    }
-
-    std::vector<float> even_fft;
-    std::vector<float> odd_fft;
-
-    fft(even, even_fft);
-    fft(odd, odd_fft);
-
-    for (int k = 0; k < N/2; k++) {
-        float theta = 2*M_PI*k/N;
-
-        float re = cos(theta);
-        float im = -sin(theta);
-
-        float re_odd = odd_fft[2*k + 0];
-        float im_odd = odd_fft[2*k + 1];
-
-        out[2*k + 0] = even_fft[2*k + 0] + re*re_odd - im*im_odd;
-        out[2*k + 1] = even_fft[2*k + 1] + re*im_odd + im*re_odd;
-
-        out[2*(k + N/2) + 0] = even_fft[2*k + 0] - re*re_odd + im*im_odd;
-        out[2*(k + N/2) + 1] = even_fft[2*k + 1] - re*im_odd - im*re_odd;
-    }
+template <typename T>
+static inline void fft_dealloc(T *ptr) {
+    return pocketfft::detail::aligned_dealloc(ptr);
 }
 
 static void log_mel_spectrogram_worker_thread(int ith, const std::vector<float> &hann, const float *samples,
                                               int n_samples, int fft_size, int fft_step, int n_threads,
                                               const whisper_filters &filters, bool speed_up, whisper_mel &mel) {
-    std::vector<float> fft_in(fft_size, 0.0);
-    std::vector<float> fft_out(2 * fft_size);
+
+    int fft_out_size = fft_size / 2 + 1;
+    float *fft_in = fft_alloc<float>(fft_size);
+
+    std::complex<float> *fft_out = fft_alloc<std::complex<float>>(fft_out_size);
+
     int n_fft = 1 + (speed_up ? fft_size / 4 : fft_size / 2);
 
-    for (int i = ith; i < mel.n_len; i += n_threads) {
-        const int offset = i * fft_step;
+    pocketfft::shape_t  fft_shape          = {(size_t)fft_size};
+    pocketfft::stride_t fft_stride_real    = {(ptrdiff_t)sizeof(float)};
+    pocketfft::stride_t fft_stride_complex = {(ptrdiff_t)sizeof(std::complex<float>)};
+
+    float max = -1e20;
+
+    int mels_to_calculate = n_samples / fft_step;
+
+    for (int i = ith; i < mels_to_calculate; i += n_threads) {
+	std::fill(fft_in, fft_in + fft_size, 0.0);
+	const int offset = i * fft_step;
 
         // apply Hanning window
         for (int j = 0; j < fft_size; j++) {
             if (offset + j < n_samples) {
                 fft_in[j] = hann[j] * samples[offset + j];
-            } else {
-                fft_in[j] = 0.0;
-            }
+	    }
         }
 
-        // FFT -> mag^2
-        fft(fft_in, fft_out);
+	pocketfft::r2c<float>(fft_shape,
+			      fft_stride_real,
+			      fft_stride_complex,
+			      0,
+			      pocketfft::FORWARD,
+			      fft_in,
+			      fft_out,
+			      1.0,
+			      1);
 
-        for (int j = 0; j < fft_size; j++) {
-            fft_out[j] = (fft_out[2 * j + 0] * fft_out[2 * j + 0] + fft_out[2 * j + 1] * fft_out[2 * j + 1]);
-        }
-        for (int j = 1; j < fft_size / 2; j++) {
-            fft_out[j] += fft_out[fft_size - j];
+        for (int j = 0; j < fft_out_size; j++) {
+	    fft_out[j] = std::norm(fft_out[j]); // This sets the imaginary part to zero
         }
 
         if (speed_up) {
             // scale down in the frequency domain results in a speed up in the time domain
-            for (int j = 0; j < n_fft; j++) {
-                fft_out[j] = 0.5 * (fft_out[2 * j] + fft_out[2 * j + 1]);
+	    for (int j = 0; j < n_fft; j+=2) {
+	      fft_out[j] = (fft_out[2 * j] + fft_out[2 * j + 1]) / (float)2.0;
             }
         }
 
         // mel spectrogram
         for (int j = 0; j < mel.n_mel; j++) {
-            double sum = 0.0;
+            float sum = 0.0;
 
             // unroll loop (suggested by GH user @lunixbochs)
             int k = 0;
             for (k = 0; k < n_fft - 3; k += 4) {
                 sum +=
-                    fft_out[k + 0] * filters.data[j*n_fft + k + 0] +
-                    fft_out[k + 1] * filters.data[j*n_fft + k + 1] +
-                    fft_out[k + 2] * filters.data[j*n_fft + k + 2] +
-                    fft_out[k + 3] * filters.data[j*n_fft + k + 3];
+		  fft_out[k + 0].real() * filters.data[j*n_fft + k + 0] +
+		  fft_out[k + 1].real() * filters.data[j*n_fft + k + 1] +
+		  fft_out[k + 2].real() * filters.data[j*n_fft + k + 2] +
+		  fft_out[k + 3].real() * filters.data[j*n_fft + k + 3];
             }
 
             // handle n_fft remainder
             for (; k < n_fft; k++) {
-                sum += fft_out[k] * filters.data[j * n_fft + k];
+	      sum += fft_out[k].real() * filters.data[j * n_fft + k];
             }
 
-            sum = log10(std::max(sum, 1e-10));
+            sum = log10(std::max(sum, max));
 
             mel.data[j * mel.n_len + i] = sum;
         }
     }
+    fft_dealloc(fft_in);
+    fft_dealloc(fft_out);
 }
 
 // ref: https://github.com/openai/whisper/blob/main/whisper/audio.py#L92-L124
@@ -4207,7 +4155,7 @@ int whisper_full_with_state(
     while (true) {
         if (params.progress_callback) {
             const int progress_cur = (100*(seek - seek_start))/(seek_end - seek_start);
-          
+
             params.progress_callback(
                 ctx, ctx->state, progress_cur, params.progress_callback_user_data);
         }
